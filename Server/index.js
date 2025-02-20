a639caa54463529581736046f779405574fe8ee5
Sohel <shaikhsohel113441@gmail.com>
2025-02-20 15:45:31 +0530

    url Changed

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // Allow only your frontend
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization"
+}));
 
 connectDB();
 
