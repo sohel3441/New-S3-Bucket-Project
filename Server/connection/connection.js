ec85ce182f13fc3729b648d1076032132a8a1303
Sohel <shaikhsohel113441@gmail.com>
2025-02-20 16:21:57 +0530

    mongo

--- Server/connection/connection.js
+++ Server/connection/connection.js
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const connectDB = async () => {
+async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
+    await mongoose.connect('mongodb://localhost:27017/File-Manager', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
   }
-};
+}
 
 export default connectDB;
-
 
 // import mongoose from 'mongoose';
 // import dotenv from 'dotenv';
