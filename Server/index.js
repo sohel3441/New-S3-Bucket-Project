1515609d5bea8a3d170ec187c73f8d98d1fb1e07
Sohel <shaikhsohel113441@gmail.com>
2025-02-23 13:32:36 +0530

    *

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  // origin: 'http://localhost:5173',
+  origin: '*',
   methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization'
 }))
