8ec0d8ee27b7e41c00b84db4d64f878552d7bd80
Sohel <shaikhsohel113441@gmail.com>
2025-02-23 13:25:46 +0530

    Error

--- Server/index.js
+++ Server/index.js
@@ -23,6 +23,8 @@ app.get('/', (req, res) => {
   res.send('API is running')
 });
 
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
